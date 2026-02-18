3b909f0ee4238111bb4616a01b0c90321d9f75c8
nyxparadox <paradoxzsite@gmail.com>
2026-02-18 16:26:56 +0530

    initialized firebase with mobile_app

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_app/Screens/authScreens/signInScreen.dart';
 import 'package:mobile_app/Services/serviceLocater.dart';
+import 'package:mobile_app/firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await setUpServiceLocater();
   runApp(const MyApp());
 }
