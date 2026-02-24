2c4fb9a48a1dad4cbbf3d0e356778f353f86057c
nyxparadox <paradoxzsite@gmail.com>
2026-02-25 00:31:48 +0530

    added cubit and repository files of notice in service locator

--- mobile_app/lib/Services/serviceLocater.dart
+++ mobile_app/lib/Services/serviceLocater.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile_app/Data/Repository/auth_repository.dart';
+import 'package:mobile_app/Data/Repository/notice_repository.dart';
 import 'package:mobile_app/Router/appRouter.dart';
 import 'package:mobile_app/logic/cubit/auth_cubit.dart';
+import 'package:mobile_app/logic/cubit/notice_cubit.dart';
 
 final getIt = GetIt.instance;
 
@@ -12,6 +14,8 @@ Future<void> setUpServiceLocater() async{
   getIt.registerLazySingleton<FirebaseFirestore>(()=> FirebaseFirestore.instance);
   getIt.registerLazySingleton<FirebaseAuth>(()=> FirebaseAuth.instance);
   getIt.registerLazySingleton(() => AuthRepository());
+  getIt.registerLazySingleton(() => NoticeRepository());
   getIt.registerLazySingleton(()=> AuthCubit(authRepository: AuthRepository()));
+  getIt.registerLazySingleton(()=> NoticeCubit(noticeRepository: NoticeRepository()));
 
 }
