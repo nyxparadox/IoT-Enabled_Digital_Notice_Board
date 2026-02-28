d77206c8fa5cf730f044afd0193537382f6eea02
nyxparadox <paradoxzsite@gmail.com>
2026-02-28 16:55:14 +0530

    file: reset_passeord dialog box for forgot password reset

---
+++ mobile_app/lib/Screens/authScreens/reset_password_dialog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_app/Services/serviceLocater.dart';
+import 'package:mobile_app/logic/cubit/auth_cubit.dart';
+
+class ResetPasswordDialog extends StatelessWidget {
+  const ResetPasswordDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _emailController = TextEditingController();
+
+    return AlertDialog(
+      title: Text("Reset Password"),
+      content: TextField(
+        controller: _emailController,
+        decoration: InputDecoration(
+          hintText: "enter your registered email",
+        ),
+      ),
+      actions: [
+        TextButton(onPressed: ()=> Navigator.pop(context),
+         child: Text("cancel")),
+         ElevatedButton(onPressed: (){
+          getIt<AuthCubit>().forgotPassword(_emailController.text);
+          Navigator.pop(context);
+         }, child: Text("Send"))
+      ],
+    );
+  }
+}
