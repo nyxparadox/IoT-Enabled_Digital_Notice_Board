56ba5c39174571c633043bcb4240afda0ea75ba7
nyxparadox <paradoxzsite@gmail.com>
2026-05-12 18:21:28 +0530

    refactor: optimize notice repository and merge notice operations

--- mobile_app/lib/Data/Repository/notice_repository.dart
+++ mobile_app/lib/Data/Repository/notice_repository.dart
@@ -1,15 +1,17 @@
 import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:mobile_app/Data/Model/notice_Model.dart';
 import 'package:mobile_app/Services/baseRepository.dart';
 
 class NoticeRepository extends Baserepository {
-  Future<NoticeModel> setNotice(
-    String category,
-    String message,
-    String? symbol,
-  ) async {
+  Future<NoticeModel> sendNotice({
+    required String category,
+    required String message,
+    required String? symbol,
+    Timestamp? expiryAt,
+  }) async {
     try {
       final currentUser = FirebaseAuth.instance.currentUser;
 
@@ -29,9 +31,16 @@ class NoticeRepository extends Baserepository {
         message: message,
         createdBy: userData['name'],
         isActive: false,
+        createdAt: Timestamp.now(),
+        expiryAt: expiryAt,     // optional 
       );
 
       final docRef = await firestore.collection("notices").add(notice.toMap());
+
+      await firestore
+         .collection("notices")
+         .doc("currentNotice")
+         .set(notice.toMap());
 
       final DatabaseReference rtdb = FirebaseDatabase.instance.ref();
 
@@ -43,6 +52,8 @@ class NoticeRepository extends Baserepository {
         "category": category,
         "message": message,
         "symbol": symbol,
+        "createdAt": Timestamp.now().millisecondsSinceEpoch,
+        "expiryAt":expiryAt?.millisecondsSinceEpoch,
       });
       
       return notice.copyWith(nid: docRef.id);
@@ -52,56 +63,17 @@ class NoticeRepository extends Baserepository {
     }
   }
 
-  Future<NoticeModel> updateNotice(
-    String category,
-    String message,
-    String? symbol,
-  ) async {
-    try {
-      final currentUser = FirebaseAuth.instance.currentUser;
-      if (currentUser == null) {
-        throw Exception("user not loged In");
-      }
-      final userDoc = await firestore
-          .collection("users")
-          .doc(currentUser.uid)
-          .get();
-      final userData = userDoc.data() as Map<String, dynamic>;
 
-      final updatedNotice = NoticeModel(
-        noticeBoardId: userData['noticeBoardId'],
-        category: category,
-        symbol: symbol,
-        message: message,
-        createdBy: userData['name'],
-        isActive: true,
-      );
-      await firestore
-          .collection("notices")
-          .doc("currentNotice")
-          .set(updatedNotice.toMap());
+// listen and stream notices from realtime databse for current dispaying message on app
+  Stream<DatabaseEvent> currentNoticeStream(
+    String boardId,
+  ) {
 
-      final DatabaseReference rtdb = FirebaseDatabase.instance.ref();
-
-       String boardId = userData['noticeBoardId']
-          .replaceAll(".", "_")
-          .replaceAll("/", "_");
-
-
-      await rtdb.child("noticeBoard").child(boardId).child("Notice").set({
-        "category": category,
-        "message": message,
-        "symbol": symbol,
-      });
-
-      final updatedNoticeDoc = await firestore
-          .collection("notices")
-          .doc("currentNotice")
-          .get();
-      return NoticeModel.fromFirestore(updatedNoticeDoc);
-    } catch (e) {
-      log("error: $e");
-      rethrow;
-    }
+    return firebaseDatabase
+        .ref()
+        .child("noticeBoard")
+        .child(boardId)
+        .child("Notice")
+        .onValue;
   }
 }
